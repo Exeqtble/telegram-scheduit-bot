91a02dd00f170258c5144a6a59cec6dab84df59c
Exeqtble <209444977+Exeqtble@users.noreply.github.com>
2025-11-03 04:00:09 +0300

    Create shedules.py

---
+++ shedules.py
@@ -0,0 +1,91 @@
+schedules = {
+    73214: { # группа
+        1: {  # неделя
+            'Monday': {
+              '12:00-11:00':'отдых'
+            },
+            'Tuesday': {
+                
+            },
+            'Wednesday': {
+               
+            },
+            'Thursday': {
+               
+            },
+            'Friday': {
+               
+            },
+            'Saturday': {
+               
+            },
+            'Sunday': 'Выходной'
+        },
+        2: {  
+            'Monday': {
+               
+            },
+            'Tuesday': {
+              
+            },
+            'Wednesday': {
+               
+            },
+            'Thursday': {
+               
+            },
+            'Friday': {
+               
+            },
+            'Saturday': {
+               
+            },
+            'Sunday': 'Выходной'
+        }
+    },
+    213157: {
+        1: {  
+            'Monday': {
+               
+            },
+            'Tuesday': {
+               
+            },
+            'Wednesday': {
+              
+            },
+            'Thursday': {
+               
+            },
+            'Friday': {
+               
+            },
+            'Saturday': {
+              
+            },
+            'Sunday': 'Выходной'
+         },
+    2: {  
+        'Monday': {
+          
+        },
+        'Tuesday': {
+           
+        },
+        'Wednesday': {
+           
+        },
+        'Thursday': {
+            
+        },
+        'Friday': {
+           
+        }
+     'Saturday': {
+              
+            },
+            'Sunday': 'Выходной'
+         },
+    }
+  }
+}
